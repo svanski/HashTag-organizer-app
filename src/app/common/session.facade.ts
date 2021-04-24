c5f04286b8468d64df9efafcf62585656120c1e3
Dachi Chkadua <dachge@gmail.com>
2021-04-24 15:39:35 -0400

    Fix error with saving undefined as a value in session store

--- src/app/common/session.facade.ts
+++ src/app/common/session.facade.ts
@@ -1,8 +1,21 @@
+type Unit<T> = {
+    content: T;
+}
+
+
 export function SessionFacade<T>(sessionId: string) {
     sessionStorage.SessionName = sessionId;
-
     return {
-        save: (item: T) => sessionStorage.setItem(sessionId, JSON.stringify(item)),
-        load: (): T | undefined => sessionStorage.getItem(sessionId) ? JSON.parse(sessionStorage.getItem(sessionId) as string) : undefined
+        save: (item: T) => sessionStorage.setItem(sessionId, JSON.stringify({ content: item } as Unit<T>)),
+       
+        load: (): T | undefined => {
+            const data = sessionStorage.getItem(sessionId);
+            if (!data) {
+                return undefined;
+            }
+
+            const unit = JSON.parse(data) as Unit<T>;
+            return unit.content;
+        }
     }
 }
